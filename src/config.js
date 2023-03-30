59ce1886963f884af202499e692b79eb4c0819b7
zorannkrstic <zorankrstic81@gmail.com>
2023-03-30 23:36:25 +0200

    gitignore

--- src/config.js
+++ src/config.js
@@ -1,3 +1,1 @@
-//export const apiKey = "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";
-
-export const apiKey = "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";
+export const apiKey = "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";
