ceea048e098027022f903c4b5c4230cce6d1169f
zorannkrstic <zorankrstic81@gmail.com>
2023-04-11 18:36:06 +0200

    auth-context

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
 import AppPage from "./pages/AppPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
+import AuthContext from "./store/auth-contex";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  const login = () => {
+    setIsLoggedIn(true);
+  };
+
+  const logout = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/app" element={<AppPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/" element={<Navigate to="/signup" />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/app" element={<AppPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/" element={<Navigate to="/signup" />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 };
 
--- src/pages/AppPage.js
+++ src/pages/AppPage.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ChatInterface from "../components/chat/chatInterface/ChatInterface";
 import MenuContainer from "../components/menu/MenuContainer";
+import AuthContext from "../store/auth-contex";
+import { Navigate } from "react-router-dom";
 
 const AppPage = (props) => {
+  const { isLoggedIn } = useContext(AuthContext);
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" />;
+  }
+
   return (
     <Container fluid className="g-0 ">
       <Row className="g-0 ">
