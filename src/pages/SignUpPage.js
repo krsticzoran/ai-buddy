16ad9488262ed9cf34a639bb3705217b72403b2c
zorannkrstic <zorankrstic81@gmail.com>
2023-04-19 11:10:20 +0200

    signUp error

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import FormContainer from "../components/form/FormContainer";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
@@ -9,9 +9,16 @@ import { Navigate } from "react-router-dom";
 
 const SignUpForm = () => {
   const authCtx = useContext(AuthContext);
+  const [signUpError, setSignUpError] = useState(null);
+  const userNameRef = useRef();
 
   const handleSignUp = async (event) => {
     event.preventDefault();
+    if (!userNameRef.current.value.trim()) {
+      setSignUpError("Please enter a username");
+      return;
+    }
+
     const { email, password, username } = event.target.elements;
     try {
       const userCredential = await createUserWithEmailAndPassword(
@@ -29,6 +36,7 @@ const SignUpForm = () => {
       authCtx.login();
     } catch (error) {
       console.error(error);
+      setSignUpError("Incorrect email or password. Please try again.");
     }
   };
   if (authCtx.isLoggedIn) {
@@ -43,7 +51,12 @@ const SignUpForm = () => {
           <label htmlFor="username" className="form-label">
             Username
           </label>
-          <input type="text" className="form-control" id="username" />
+          <input
+            ref={userNameRef}
+            type="text"
+            className="form-control"
+            id="username"
+          />
         </div>
         <div className="mb-3">
           <label htmlFor="email" className="form-label">
@@ -57,6 +70,11 @@ const SignUpForm = () => {
           </label>
           <input type="password" className="form-control" id="password" />
         </div>
+        {signUpError && (
+          <div className="alert alert-danger" role="alert">
+            {signUpError}
+          </div>
+        )}
         <div className="d-grid gap-2">
           <button type="submit" className="btn btn-primary">
             Sign up
