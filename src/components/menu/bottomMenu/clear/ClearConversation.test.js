49bad1033243fecc776ab3f223eb8102d8609d81
zorannkrstic <zorankrstic81@gmail.com>
2023-05-30 19:50:44 +0200

    clear test

--- src/components/menu/bottomMenu/clear/ClearConversation.test.js
+++ src/components/menu/bottomMenu/clear/ClearConversation.test.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
 import user from '@testing-library/user-event';
 import { ChatContext } from '../../../../store/chat-context';
-
+import { newChatActions } from '../../../../store/startNewChat';
 const mockStore = configureMockStore();
 const store = mockStore({
   auth: { uid: 'user123' },
@@ -16,7 +16,10 @@ const chatCtx = {
   addTitle: jest.fn(),
 };
 
-test('renders clear button', async () => {
+test('renders clear button and triggers necessary actions on click', async () => {
+  const dispatchMock = jest.fn();
+  const originalDispatch = store.dispatch;
+  store.dispatch = dispatchMock;
   render(
     <Provider store={store}>
       <ChatContext.Provider value={chatCtx}>
@@ -30,7 +33,10 @@ test('renders clear button', async () => {
 
   await waitFor(() => {
     expect(chatCtx.addTitle).toHaveBeenCalled();
+    expect(dispatchMock).toHaveBeenCalledWith(newChatActions.start());
   });
 
   expect(clearButton).toBeInTheDocument();
+  // Restore the original dispatch function
+  store.dispatch = originalDispatch;
 });
