2d56fa09804e2ac830eaf425f6c037210c24fd31
zorannkrstic <zorankrstic81@gmail.com>
2023-05-30 19:45:00 +0200

    logout test

--- src/components/menu/bottomMenu/logout/Logout.test.js
+++ src/components/menu/bottomMenu/logout/Logout.test.js
@@ -1,9 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
+import { ChatContext } from '../../../../store/chat-context';
 
 import user from '@testing-library/user-event';
 import Logout from './Logout';
+import { authActions } from '../../../../store/auth';
 
 const mockStore = configureMockStore();
 const store = mockStore({
@@ -11,15 +13,35 @@ const store = mockStore({
   newChat: { isNewChat: false },
 });
 
-test('renders logout button', () => {
-  const mock = jest.fn();
+// Mock the ChatContext
+const chatCtx = {
+  chatHandler: jest.fn(),
+};
+
+test('renders logout button', async () => {
+  const dispatchMock = jest.fn();
+  const originalDispatch = store.dispatch;
+  store.dispatch = dispatchMock;
+
   render(
     <Provider store={store}>
-      <Logout onClick={mock} />
+      <ChatContext.Provider value={chatCtx}>
+        <Logout />
+      </ChatContext.Provider>
     </Provider>
   );
 
   const logoutButton = screen.getByText(/Log out/i);
 
+  user.click(logoutButton);
+
+  await waitFor(() => {
+    expect(chatCtx.chatHandler).toHaveBeenCalled();
+    expect(dispatchMock).toHaveBeenCalledWith(authActions.logout());
+  });
+
   expect(logoutButton).toBeInTheDocument();
+
+  // Restore the original dispatch function
+  store.dispatch = originalDispatch;
 });
