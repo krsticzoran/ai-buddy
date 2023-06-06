46e648b09270abbba5a0b3aec73ea1861d8844fd
zorannkrstic <zorankrstic81@gmail.com>
2023-06-06 18:30:31 +0200

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAlOk--pqjGzTXVpClGeOoRf9YwLoxkd1U',
-  authDomain: 'ai-buddy-d160e.firebaseapp.com',
-  databaseURL: 'https://ai-buddy-d160e-default-rtdb.firebaseio.com',
-  projectId: 'ai-buddy-d160e',
-  storageBucket: 'ai-buddy-d160e.appspot.com',
-  messagingSenderId: '50637421188',
-  appId: '1:50637421188:web:04d6f228dffcbf1fd620d1',
-  measurementId: 'G-PQJ2PV4LN7',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
